7cc67d0dbf0c9c06a5d227f68a942c15af8bbd86
Federico Fantacone <fede26f@hotmail.com>
2022-10-30 17:01:16 -0300

    Agregar archivos de proyecto.

---
+++ WindowsAppPractica2/Entidades/Producto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAppPractica2.Entidades
+{
+    public class Producto
+    {
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }  
+        public decimal Precio { get; set; } 
+        public string color { get; set; }   
+        public string imagen { get; set; }
+        public string NombreDeCategoria { get; set; }   
+
+    }
+}
---
+++ WindowsAppPractica2/Entidades/Proveedor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsAppPractica2.Entidades
+{
+    public class Proveedor
+    {
+        public string nombre { get; set; }
+        public string direccion { get; set; }
+        public string NombreContacto { get; set; }
+        public int Telefono { get; set; }
+        public string SitioWeb { get; set; }
+
+    }
+}
